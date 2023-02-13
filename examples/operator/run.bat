c4fe259b39e0c3c2652a4612589527928137402b
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2023-02-13 23:54:26 +0800

    Delete examples directory

--- examples/operator/run.bat
+++
@@ -1,6 +0,0 @@
-set filename=main
-g++ -finput-charset=UTF-8 -fexec-charset=gbk ^
-    "%filename%.cpp" -o "%filename%.exe" ^
-    -lglfw3 -lgdi32 -lopengl32 -lglew32 -lfreeglut -lsfml-graphics -lsfml-window -lsfml-system ^
-    -I E:/C++/Project_Neutron/.release/
-"%filename%.exe"
